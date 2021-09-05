891912e3d5a76c7779bba7bc53d198bed3285de2
serhatdalcicek <serhat21444@gmail.com>
2021-09-05 18:04:29 +0300

    First Commit

---
+++ Paket1/index.js
@@ -0,0 +1,5 @@
+console.log("Paket1");
+
+const is = require("simple-is");
+
+console.log(is.number(24));
---
+++ Paket2/index.js
@@ -0,0 +1,5 @@
+console.log("Paket2");
+
+const is = require("simple-is");
+
+console.log(is.number(24));
